5fda0dd8a58f6655ee6fadab9e0f02ceb58dd2e4
Ikenna Oparaugo <ikenna.oparaugo@pwc.com>
2022-10-02 12:57:00 +0400

    committing changes

--- STA.VisitorCareCRM/Plugins/Presenters/RestrictModClosurePresenter.cs
+++ STA.VisitorCareCRM/Plugins/Presenters/RestrictModClosurePresenter.cs
@@ -18,36 +18,44 @@ namespace STA.TouristCareCRM.Plugins.Presenters
         {
             Entity entTargetCaseApproval = commonMethods.RetrieveTargetEntityFromContext(objCommonForAllPlugins.pluginContext);
             Entity entPreImageCaseApproval = commonMethods.RetrievePreImageEntityFromContext(objCommonForAllPlugins.pluginContext);
-
+            string regardingObjectLogicalName = string.Empty;
             if (entTargetCaseApproval != null)
             {
                 EntityReference entrefRegardingObject = commonMethods.GetAttributeValFromTargetOrImageEntities(entTargetCaseApproval, entPreImageCaseApproval, CaseApprovalEntityAttributeNames.RegardingObject);
                 EntityReference entrefRelatedCaseObject = commonMethods.GetAttributeValFromTargetOrImageEntities(entTargetCaseApproval, entPreImageCaseApproval, CaseApprovalEntityAttributeNames.RelatedCase);
                 OptionSetValue optsetRequestType = commonMethods.GetAttributeValFromTargetOrImageEntities(entTargetCaseApproval, entPreImageCaseApproval, CaseApprovalEntityAttributeNames.RequestType);
 
-                if (entrefRegardingObject != null && !string.IsNullOrWhiteSpace(entrefRegardingObject.LogicalName) || entrefRelatedCaseObject!=null && !string.IsNullOrWhiteSpace(entrefRelatedCaseObject.LogicalName))
-                {
-                    switch (entrefRelatedCaseObject.LogicalName)
+                    if (entrefRegardingObject != null && !string.IsNullOrWhiteSpace(entrefRegardingObject.LogicalName)) { 
+                        regardingObjectLogicalName = entrefRegardingObject.LogicalName;
+                    } 
+
+                    else if(entrefRelatedCaseObject != null && !string.IsNullOrEmpty(entrefRelatedCaseObject.LogicalName))
+                    {
+                        regardingObjectLogicalName = entrefRelatedCaseObject.LogicalName;
+                    }
+
+
+                    switch (regardingObjectLogicalName)
                     {
                         case CaseEntityAttributeNames.EntityLogicalName:
                             if (optsetRequestType != null && optsetRequestType.Value != int.MinValue && optsetRequestType.Value == (int)RequestType.MomentofDelight)
                             {
                                 //Get the role Id from the configparameter - VC Quality Assurance
-                                string query = cRMFetch.GetRequiredConfigParamRecord("ModRoleId");
-                                string roleId = string.Empty;
+                                string query = cRMFetch.GetRequiredConfigParamRecord("MoDRoleName");
+                                string roleName = string.Empty;
                                 if (!string.IsNullOrWhiteSpace(query))
                                 {
                                     EntityCollection ModRole = objCommonForAllPlugins.srvContextUsr.RetrieveMultiple(new FetchExpression(query));
                                     if (ModRole != null && ModRole.Entities != null && ModRole.Entities.Count > 0)
                                     {
-                                        roleId = ModRole.Entities[0].GetAttributeValue<string>(ConfigParamsEntityAttributeNames.ConfigParamValue);
+                                        roleName = ModRole.Entities[0].GetAttributeValue<string>(ConfigParamsEntityAttributeNames.ConfigParamValue);
                                     }
 
                                     //Get the initiating users security roles and check if the user has the VC Quality Assurance role
                                     EntityCollection userRoles = commonMethods.GetUserRoles(objCommonForAllPlugins.srvContextUsr, objCommonForAllPlugins.pluginContext.InitiatingUserId);
                                     if (userRoles != null && userRoles.Entities != null && userRoles.Entities.Count > 0)
                                     {
-                                       hasRole = userRoles.Entities.Any(y => y.Attributes.Contains(RoleEntityAttributeNames.RoleId) && ((Guid)y.Attributes[RoleEntityAttributeNames.RoleId]).ToString().ToLower().Trim() == roleId.ToLower().Trim());
+                                       hasRole = userRoles.Entities.Any(y => y.Attributes.Contains(RoleEntityAttributeNames.Name) && ((string)y.Attributes[RoleEntityAttributeNames.Name]).ToString().ToLower().Trim() == roleName.ToLower().Trim());
 
                                         if (!hasRole)
                                         {
@@ -64,7 +72,7 @@ namespace STA.TouristCareCRM.Plugins.Presenters
 
                             break;
                     }
-                }
+                
 
             }
 
